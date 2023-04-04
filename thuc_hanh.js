ceb8ae31af705847f86fd7113a7148f4c549d13d
lochoang003 <129358033+lochoang003@users.noreply.github.com>
2023-04-04 21:07:49 +0700

    thuc_hanh_javarsp

---
+++ thuc_hanh.js
@@ -0,0 +1,12 @@
+function aLert(){
+    alert('Đât là Alert');
+}
+function conFirm(){
+    confirm("đây là Confirm")
+}
+function Witre() {
+    document.write("đây là Write")
+}
+function conSole() {
+    console.log("xin chao")
+}
