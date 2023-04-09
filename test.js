c3064dccc8cf7833a9c1e2c3256f66ee94523ae4
lochoang003 <129358033+lochoang003@users.noreply.github.com>
2023-04-09 14:16:41 +0700

    bai tap

---
+++ test.js
@@ -0,0 +1,14 @@
+function abc(){
+let a
+let b
+a = +prompt("nhap gia tri a")
+b = +prompt("nhap gia tri b")
+document.write("gia tri cua a = " +a )
+document.write("<br/>")
+document.write("gia tri cua a = " +b )
+document.write("<br>")
+let c
+c= a*b
+document.write("gia tri c = " + c)
+
+}
---
+++ toan_tu.js
@@ -0,0 +1,10 @@
+inputRong = +prompt("chiều rộng");
+inputDai = +prompt("chiều dài");
+let rong = inputRong;
+let dai= inputDai;
+let S = rong*dai
+document.write("chiều dài là : " + dai)
+document.write("<br>")
+document.write("chiều rộng là : " + rong)
+document.write("<br>")
+document.write("diện tích S =" + S)
